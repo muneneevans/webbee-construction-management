7a0ca9019e8c0db2de1b5814f3c3dad0d8a73bca
muneneevans <evans@vorane.com>
2023-01-24 12:10:50 +0300

    Set up drawer

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 import {AppRegistry} from 'react-native';
-// import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
 import App from './src';
 import {name as appName} from './app.json';
 
--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -5,29 +5,31 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Dashboard from 'screens/Dashboard/Dashboard';
 import Categories from 'src/Screens/Categories/Categories';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import {View, Text} from 'react-native';
+import {useWindowDimensions} from 'react-native';
 
-// type RootDrawerParamList = {
-//   Dashboard: {
-//     title: string;
-//   };
-//   Categories: {
-//     title: string;
-//   };
-// };
+type RootDrawerParamList = {
+  Dashboard: {
+    title: string;
+  };
+  Categories: {
+    title: string;
+  };
+};
 
 // const Stack = createNativeStackNavigator<RootDrawerParamList>();
-// const Drawer = createDrawerNavigator<RootDrawerParamList>();
+const Drawer = createDrawerNavigator<RootDrawerParamList>();
 // const Drawer = createDrawerNavigator();
 function App() {
+  const dimensions = useWindowDimensions();
+
   return (
-    // <Drawer.Navigator>
-    //   <Drawer.Screen name="Dashboard" component={Dashboard} />
-    //   <Drawer.Screen name="Categories" component={Categories} />
-    // </Drawer.Navigator>
-    <View>
-      <Text>Routes</Text>
-    </View>
+    <Drawer.Navigator
+      screenOptions={{
+        drawerType: dimensions.width >= 768 ? 'permanent' : 'front',
+      }}>
+      <Drawer.Screen name="Dashboard" component={Dashboard} />
+      <Drawer.Screen name="Categories" component={Categories} />
+    </Drawer.Navigator>
   );
 }
 
